0e79c7ad132966cba68194cf9c764e8a302d02f2
AlbertsDiterihs <a.diterihs@gmail.com>
2017-08-23 20:29:12 +0300

    Added map page and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,65 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import {HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule, Component} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {HttpModule} from '@angular/http';
+import {
+    GoogleMaps
+} from '@ionic-native/google-maps';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-import { GamePage } from '../pages/game/game';
-import { TeamDetailPage } from '../pages/team-detail/team-detail';
-import { TournamentsPage } from '../pages/tournaments/tournaments';
-import { MyTeamsPage } from '../pages/my-teams/my-teams';
-import { TeamsPage } from '../pages/teams/teams';
-import { StandingsPage } from '../pages/standings/standings';
-import { TeamHomePage } from '../pages/team-home/team-home';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {ListPage} from '../pages/list/list';
+import {GamePage} from '../pages/game/game';
+import {TeamDetailPage} from '../pages/team-detail/team-detail';
+import {TournamentsPage} from '../pages/tournaments/tournaments';
+import {MyTeamsPage} from '../pages/my-teams/my-teams';
+import {TeamsPage} from '../pages/teams/teams';
+import {StandingsPage} from '../pages/standings/standings';
+import {TeamHomePage} from '../pages/team-home/team-home';
+import {MapPage} from '../pages/map/map';
 // import { GamePage } from '../pages/game/game';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    ListPage,
-    MyTeamsPage,
-    GamePage,
-    TeamDetailPage,
-    TournamentsPage,
-    TeamsPage,
-    StandingsPage,
-    TeamHomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-    HttpModule
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage,
-    MyTeamsPage,
-    GamePage,
-    TeamDetailPage,
-    TournamentsPage,
-    TeamsPage,
-    StandingsPage,
-    TeamHomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+        ListPage,
+        MyTeamsPage,
+        GamePage,
+        TeamDetailPage,
+        TournamentsPage,
+        TeamsPage,
+        StandingsPage,
+        TeamHomePage,
+        MapPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp),
+        HttpModule
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        ListPage,
+        MyTeamsPage,
+        GamePage,
+        TeamDetailPage,
+        TournamentsPage,
+        TeamsPage,
+        StandingsPage,
+        TeamHomePage,
+        MapPage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler},
+        GoogleMaps
+    ]
 })
 export class AppModule {}
